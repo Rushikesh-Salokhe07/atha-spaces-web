282bedf88c4bd9156f9f5422694a90d4ebf55555
Rishikesh Salokhe <salokherushikesh171@gmail.com>
2026-07-22 16:41:44 +0530

    Fix bufferAttribute args prop for r3f v9

--- src/components/3d/HeroScene.tsx
+++ src/components/3d/HeroScene.tsx
@@ -133,6 +133,7 @@ function Particles() {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
+          args={[positions, 3]}
           count={count}
           array={positions}
           itemSize={3}
